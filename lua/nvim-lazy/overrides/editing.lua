8a0b5b6323d053a02c167cdf6791ad88b6fe2dea
Tanmay Vijayvargiya <12tanmayvijay@gmail.com>
2023-08-28 02:24:20 +0530

    Add undo tree

--- lua/nvim-lazy/overrides/editing.lua
+++ lua/nvim-lazy/overrides/editing.lua
@@ -1,12 +1,30 @@
 return {
   {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {"nvim-lua/plenary.nvim", "debugloop/telescope-undo.nvim"},
+    config = function(_, opts)
+      opts = vim.tbl_deep_extend("force", opts, {
+        extensions = {
+          undo = {
+            side_by_side = true,
+            layout_strategy = "vertical",
+            layout_config = {preview_height = 0.8}
+          }
+        }
+      })
+      require("telescope").setup {opts}
+      require("telescope").load_extension("undo")
+    end,
+    keys = {
+      {"<leader>ut", "<cmd>Telescope undo", desc = "Open undo tree"}
+    }
+  },
+  {
     name = "nvim-lazy-editing",
     dir = "/dev/null",
     config = function() vim.cmd("packadd cfilter") end
   },
-  {
-    "numToStr/Comment.nvim", opts = {}
-  },
+  {"numToStr/Comment.nvim", opts = {}},
   {"windwp/nvim-autopairs", opts = {}},
   {"tanmayv/harpoon", opts = {}}
 }
