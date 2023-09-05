2041413ec3aeab343a694c6b24a41ef2683448e6
Tanmay Vijayvargiya <12tanmayvijay@gmail.com>
2023-09-06 01:20:19 +0530

    Add chatapt, hardtime, and keymaps for better navigations

--- lua/nvim-lazy/overrides/navigation.lua
+++ lua/nvim-lazy/overrides/navigation.lua
@@ -1,9 +1,18 @@
 return {
+  {
+    "m4xshen/hardtime.nvim",
+    dependencies = {"MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim"},
+    opts = {}
+  },
   "christoomey/vim-tmux-navigator",
   {
     "ahmedkhalf/project.nvim",
     main = "project_nvim",
-    opts = {detection_methods = {"pattern", "lsp"}, dpatterns = {"BUILD"}, silent_chdir = false }
+    opts = {
+      detection_methods = {"pattern", "lsp"},
+      dpatterns = {"BUILD"},
+      silent_chdir = false
+    }
   },
   {
     "tanmayv/harpoon",
@@ -22,7 +31,7 @@ return {
         "\\c",
         function() require("harpoon.cmd-ui").toggle_quick_menu() end,
         desc = "[Harpoon] Cmd quick menu"
-      },
+      }
     }
   },
   {
@@ -30,7 +39,18 @@ return {
     opts = {},
     keys = {
       {"-", "<cmd>Oil<cr>", desc = "Open current file's folder"},
-      {"_", "40<c-w>v<cmd>Oil<cr>", desc = "Open current file's folder"},
+      {"_", "40<c-w>v<cmd>Oil<cr>", desc = "Open current file's folder"}
+    }
+  },
+  {
+    "tabs_navigation",
+    dir = "/dev/null",
+    dependencies = {"ton/vim-bufsurf"},
+    keys = {
+      {"J", "gT", desc = "Go to previous tab"},
+      {"K", "gt", desc = "Go to next tab"},
+      {"H", "<cmd>BufSurfBack<cr>", desc = "Go to previous in buffer history"},
+      {"L", "<cmd>BufSurfForward<cr>", desc = "Go to next in buffer history"},
     }
   }
 }
--- lua/nvim-lazy/overrides/options.lua
+++ lua/nvim-lazy/overrides/options.lua
@@ -3,7 +3,8 @@ return {
     name = "nvim-lazy-options",
     dir = "/dev/null",
     config = function()
-	vim.o.number = true
+	vim.wo.number = true
+	vim.wo.relativenumber = true
 	vim.o.shiftwidth = 2
 	vim.o.smarttab = true
     end
