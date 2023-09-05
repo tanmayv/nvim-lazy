f1c2b2f6e2793b2276c573e72694af6d53718965
Tanmay Vijayvargiya <12tanmayvijay@gmail.com>
2023-09-06 01:45:29 +0530

    Add gkeep

---
+++ lua/nvim-lazy/overrides/ai.lua
@@ -0,0 +1,7 @@
+-- lazy.nvim
+return {
+  {
+    "robitx/gp.nvim", opts = {}
+  }
+}
+
---
+++ lua/nvim-lazy/overrides/notes.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    'stevearc/gkeep.nvim',
+    build = "UpdateRemotePlugins",
+    opts = {},
+    -- Optional dependencies
+    dependencies = {"nvim-tree/nvim-web-devicons"}
+  }
+}
