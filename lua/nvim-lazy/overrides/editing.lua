41d7cdd86135ffd3702e7fc34735b6f9ce4ce8f7
Tanmay Vijayvargiya <12tanmayvijay@gmail.com>
2023-09-06 14:38:25 +0530

    Add fzf for telescope

--- lua/nvim-lazy/overrides/editing.lua
+++ lua/nvim-lazy/overrides/editing.lua
@@ -4,51 +4,41 @@ local Input = function(on_submit, prompt)
 
   local input = Input({
     position = "50%",
-    size = {
-      width = 30,
-    },
-    border = {
-      style = "single",
-      text = {
-	top = "Find",
-	top_align = "center",
-      },
-    },
-    win_options = {
-      winhighlight = "Normal:Normal,FloatBorder:Normal",
-    },
+    size = {width = 30},
+    border = {style = "single", text = {top = "Find", top_align = "center"}},
+    win_options = {winhighlight = "Normal:Normal,FloatBorder:Normal"}
   }, {
     prompt = ">",
     default_value = "",
-    on_close = function()
-    end,
-    on_submit = on_submit,
+    on_close = function() end,
+    on_submit = on_submit
   })
 
   -- mount/open the component
   input:mount()
 
   -- unmount component when cursor leaves buffer
-  input:on(event.BufLeave, function()
-    input:unmount()
-  end)
+  input:on(event.BufLeave, function() input:unmount() end)
 end
 
-local FindReplace = function ()
-  Input(function(find_value) 
-    Input(function(replace_value) 
+local FindReplace = function()
+  Input(function(find_value)
+    Input(function(replace_value)
       require("smart-replace").ReplaceFile(find_value, replace_value)
     end, "Replace")
   end, "Find")
 end
 
-local FindReplaceFile = function ()
-end
+local FindReplaceFile = function() end
 
 return {
   {
     "nvim-telescope/telescope.nvim",
-    dependencies = {"nvim-lua/plenary.nvim", "debugloop/telescope-undo.nvim"},
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "debugloop/telescope-undo.nvim",
+      {"nvim-telescope/telescope-fzf-native.nvim", build = "make"}
+    },
     config = function(_, opts)
       opts = vim.tbl_deep_extend("force", opts, {
         extensions = {
@@ -56,24 +46,26 @@ return {
             side_by_side = true,
             layout_strategy = "vertical",
             layout_config = {preview_height = 0.8}
+          },
+          fzf = {
+            fuzzy = true, -- false will only do exact matching
+            override_generic_sorter = true, -- override the generic sorter
+            override_file_sorter = true, -- override the file sorter
+            case_mode = "smart_case" -- or "ignore_case" or "respect_case"
+            -- the default case_mode is "smart_case"
           }
         }
       })
       require("telescope").setup {opts}
       require("telescope").load_extension("undo")
+      require("telescope").load_extension("fzf")
     end,
-    keys = {
-      {"<leader>ut", "<cmd>Telescope undo", desc = "Open undo tree"}
-    }
+    keys = {{"<leader>ut", "<cmd>Telescope undo", desc = "Open undo tree"}}
   },
   {
     "tanmayv/smart-replace",
     dependencies = {"MunifTanjim/nui.nvim"},
-    keys = {
-      {
-      "<leader>rf", FindReplace, desc = "Smart find replace on file"
-      }
-    }
+    keys = {{"<leader>rf", FindReplace, desc = "Smart find replace on file"}}
   },
   {'kevinhwang91/nvim-bqf', ft = 'qf'},
   {
