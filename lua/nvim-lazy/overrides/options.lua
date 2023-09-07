dc6c76c0af6bf9af136770a523d4059dc38b9ad8
Tanmay Vijayvargiya <12tanmayvijay@gmail.com>
2023-09-07 15:06:43 +0530

    Revert mapping ; to :

--- lua/nvim-lazy/overrides/options.lua
+++ lua/nvim-lazy/overrides/options.lua
@@ -3,11 +3,19 @@ return {
     name = "nvim-lazy-options",
     dir = "/dev/null",
     config = function()
-	vim.wo.number = true
-	vim.wo.relativenumber = true
-	vim.o.shiftwidth = 2
-	vim.o.smarttab = true
+      vim.wo.number = true
+      vim.wo.relativenumber = true
+      vim.o.shiftwidth = 2
+      vim.o.smarttab = true
     end
+  },
+  {
+    name = "nvgoog-mappings",
+    dir = "/dev/null",
+    keys = {
+      -- Remap ; tp ; for faster command access
+      {";", ";", mode = {"n", "v"}}
+    }
   }
 }
 
